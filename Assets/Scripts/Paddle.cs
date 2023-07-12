5cc89eb56c6faac971ebdd171d3de11938e35887
o-lacueva <139348785+o-lacueva@users.noreply.github.com>
2023-07-12 15:40:57 +0100

    initial commit
    starter breakout game imported

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float Speed = 1.5f;
+    public float MaxMovement = 1.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+
+        Vector3 pos = transform.position;
+        pos.x += input * Speed * Time.deltaTime;
+
+        if (pos.x > MaxMovement)
+            pos.x = MaxMovement;
+        else if (pos.x < -MaxMovement)
+            pos.x = -MaxMovement;
+
+        transform.position = pos;
+    }
+}
